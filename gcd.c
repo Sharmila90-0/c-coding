7f01054662f98185d957d53427bcefd0065cf225
Sharmila90-0 <sharmilasaha910@gmail.com>
2026-02-05 15:37:06 +0530

    Create gcd.c

---
+++ gcd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a,b, gcd=1;// Write C code here
+    printf("enter two integers:");
+    scanf("%d%d",&a,&b);
+    int c=(a<b)?a:b;
+    for(int i=1;i<=c;i++){
+        if (a%i==0&&b&i==0)
+        gcd=i;
+    }
+printf("GCD of %d and %d is:%d" ,a,b,gcd);
+    return 0;
+}
