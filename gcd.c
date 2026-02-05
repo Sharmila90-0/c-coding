08e93799bdf010ba3518a177af524177e2ffec9c
Sharmila90-0 <sharmilasaha910@gmail.com>
2026-02-05 15:40:36 +0530

    Update gcd.c

--- gcd.c
+++ gcd.c
@@ -6,7 +6,7 @@ int main() {
     scanf("%d%d",&a,&b);
     int c=(a<b)?a:b;
     for(int i=1;i<=c;i++){
-        if (a%i==0&&b&i==0)
+        if (a%i==0 && b%i==0)
         gcd=i;
     }
 printf("GCD of %d and %d is:%d" ,a,b,gcd);
