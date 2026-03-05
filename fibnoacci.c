7e544930aad445723dfb575b7463713e10af45c8
Sharmila90-0 <sharmilasaha910@gmail.com>
2026-03-05 15:28:11 +0530

    Create fibnoacci.c

---
+++ fibnoacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+ int a=0;
+ int b=1;
+ int n;
+    printf("Enter a series:");
+    scanf("%d",&n);
+    printf("%d%d",a,b);
+for (int i=3;i<=n;i++ )// W
+{
+int term =a+b;
+a=b;
+b=term;
+printf("%d",term);
+}
+
+    return 0 ;
+
+}
